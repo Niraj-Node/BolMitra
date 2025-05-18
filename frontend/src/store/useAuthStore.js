603d3981b844f5ea79d2d6a6370f84c77eb7f564
Niraj-Node <nirajaghera999@gmail.com>
2025-05-19 01:17:26 +0530

    Impl: getOnlineUsers Event Emit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../utils/axios.js";
 import { toast } from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_SERVER_BASE_URL_DEV: import.meta.env.VITE_SERVER_BASE_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -97,8 +97,16 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL);
+    const socket = io(BASE_URL, {
+      query: {
+        userId: authUser._id,
+      },
+    });
     socket.connect();
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+    
     set({ socket: socket });
   },
   disconnectSocket: () => {
