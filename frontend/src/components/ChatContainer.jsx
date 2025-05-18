c65dd05306867bfd4bd973e181a7128031339d78
Niraj-Node <nirajaghera999@gmail.com>
2025-05-19 00:23:21 +0530

    Impl: ChatContainer Component Complete

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,12 @@
 import { useChatStore } from "../store/useChatStore";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../utils/time";
+import MediaPreviewModal from "./MediaPreviewModal";
 
 const ChatContainer = () => {
   const {
@@ -16,8 +17,12 @@ const ChatContainer = () => {
   } = useChatStore();
   const { authUser } = useAuthStore();
 
+  const [previewImage, setPreviewImage] = useState(null);
+
   useEffect(() => {
-    getMessages(selectedUser._id);
+    if (selectedUser?._id) {
+      getMessages(selectedUser._id);
+    }
   }, [selectedUser._id, getMessages]);
 
   if (isMessagesLoading) {
@@ -34,10 +39,51 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
-      <p>messages....</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2 cursor-pointer"
+                  onClick={() => setPreviewImage(message.image)}
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
 
       <MessageInput />
+
+      {previewImage && (
+        <MediaPreviewModal image={previewImage} onClose={() => setPreviewImage(null)} />
+      )}
     </div>
   );
 };
+
 export default ChatContainer;
